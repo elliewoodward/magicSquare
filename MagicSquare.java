d6a56ca98ecb041a266daf17ea24959284cfefb1
Ellie Woodward <elliewoodward@u.boisestate.edu>
2024-01-18 14:02:07 -0700

    updated

--- MagicSquare.java
+++ MagicSquare.java
@@ -11,36 +11,36 @@ public class MagicSquare implements MagicSquareInterface {
     private int dimension;
     private String fileName;
     private int[][] array;
+    private boolean magicList;
+    private int magicNumber;
 
-
-    /* create a private void method outside of the constructor that 
-     * will read through the file, and then make sure to run the 
+    /*
+     * create a private void method outside of the constructor that
+     * will read through the file, and then make sure to run the
      * method within the constructor
      */
-    public MagicSquare(String file){
-            this.fileName = fileName;
-            File scanFile = new File(file);
-            try{
-                int[][] magicSquare = readMatrix(file);
-                this.array = magicSquare;
-            }
+    public MagicSquare(String file) throws FileNotFoundException {
+        this.fileName = fileName;
+        File scanFile = new File(file);
+        int[][] magicSquare = readMatrix(file);
+        this.array = magicSquare;
 
-            //ask for help!
-            catch(FileNotFoundException e){
-                System.out.print("File Not Found");
-            }
+        this.magicNumber = (int) ((dimesnions)*(Math.pow((double) dimension, 2.0) + 1)/2);
+
     }
 
-    /*running the correct matrix*/
-    public MagicSquare(String file, int dimension){
+    /* running the correct matrix */
+    public MagicSquare(String file, int dimension) {
         createMagicSquare(file);
+        this.magicNumber = (int) ((dimesnions)*(Math.pow((double) dimension, 2.0) + 1)/2);
     }
 
-    /* constructor to make the magic square, have algorithm from the
-     * assignment, run the right matrix --> make a private void method 
+    /*
+     * constructor to make the magic square, have algorithm from the
+     * assignment, run the right matrix --> make a private void method
      * for that. turn 2D array into file
      */
-    public void createMagicSquare(String File){
+    public void createMagicSquare(String File) {
         int n = dimension;
         int[][] array = new int[n][n];
         int row;
@@ -49,19 +49,19 @@ public class MagicSquare implements MagicSquareInterface {
         col = n / 2;
         int oldrow;
         int oldcol;
-        for(int i = 0; i < Math.pow(n, 2); i++){
+        for (int i = 0; i <= Math.pow(n, 2); i++) {
             array[row][col] = i;
             oldrow = row;
             oldcol = col;
             row++;
             col++;
-            if(row == n){
+            if (row == n) {
                 n = 0;
             }
-            if(col == n){
+            if (col == n) {
                 n = 0;
             }
-            if(array[row][col] != 0){
+            if (array[row][col] != 0) {
                 row = oldrow;
                 col = oldcol;
                 row--;
@@ -69,59 +69,101 @@ public class MagicSquare implements MagicSquareInterface {
         }
     }
 
-
-    /* this is the priavte void method that is being created to read 
+    /*
+     * this is the priavte void method that is being created to read
      * through the file and then will be run in the magic square constructor
      */
-    private int[][] readMatrix(String file){
+    private int[][] readMatrix(String file) throws FileNotFoundException {
         File scanFile = new File(file);
         int[][] magicSquare = new int[dimension][dimension];
-
-
-             try{
-                Scanner fileScan = new Scanner(scanFile);
-                while(fileScan.hasNextLine()){
-                    String line = fileScan.nextLine();
-                    Scanner lineScan = new Scanner(line);
-                    final String DELIMITERS = " ";
-                    lineScan.useDelimiter(DELIMITERS);
-                }
-            }
-            catch(FileNotFoundException e){
-                System.out.print("File Not Found");
-            }
-            return magicSquare;
-    }
-
-    private void createMatrix(int[][] matrix, String filename){
-        try{
-            File file = new File(filename);
-            PrintWriter pw = new PrintWriter(new FileWriter(file));
-            for (int i = 0; i < dimension; i++){
-                pw.println();
-                for(int j = 0; j < dimension; i++){
-                    String value = String.valueOf(array[i][j]);
-                    pw.print(value);
-                }
-            }
-            pw.close();
-        } catch (IOException e) {
-            System.out.println("IOExeception");
+        Scanner fileScan = new Scanner(scanFile);
+        while (fileScan.hasNextLine()) {
+            String line = fileScan.nextLine();
+            Scanner lineScan = new Scanner(line);
+            final String DELIMITERS = " ";
+            lineScan.useDelimiter(DELIMITERS);
         }
 
+        return magicSquare;
     }
 
+    private void createMatrix(int[][] matrix, String filename) throws IOException {
+
+        File file = new File(filename);
+        PrintWriter pw = new PrintWriter(new FileWriter(file));
+        for (int i = 0; i < dimension; i++) {
+            pw.println();
+            for (int j = 0; j < dimension; i++) {
+                String value = String.valueOf(array[i][j]);
+                pw.print(value);
+            }
+        }
+        pw.close();
+
+    }
 
     @Override
     public boolean isMagicSquare() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isMagicSquare'");
+        this.magicList = true;
+
+        for (int i = 0; i < dimension; i++) {
+            int count = 0;
+
+            for (int j = 0; j < dimension; j++) {
+                count += array[i][j];
+            }
+            if(count != magicNumber){
+                magicList = false;
+            }
+
+            for (int j = 0; j < dimension; j++) {
+                count += array[j][i];
+            }
+            if(count != magicNumber){
+                magicList = false;
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i< dimension; i++){
+            count += array[i][i];
+        }
+        if(count != magicNumber){
+            magicList = false;
+        }
+        for(int j = 0; j< dimension; j++){
+            count += array[j][j];
+        }
+        if(count != magicNumber){
+            magicList = false;
+        }
+
+        count = 0;
+        for(int i = 0; i< dimension; i++){
+            for(int j = 0; j < dimension; j++){
+                count += array[i][j];
+            }
+        }
+        
+        int test = 0;
+        for(int i = 0; i < dimension*dimension; i++){
+            test += i;
+        }
+        if(count != test){
+            magicList = false;
+        }
+
+        return magicList;
     }
 
     @Override
     public int[][] getMatrix() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMatrix'");
+        int[][] anotherArray = new int[dimension][dimension];
+        for(int i = 0; i < dimension; i++){
+            for(int j = 0; j < dimension; j++){
+                
+            }
+        }
     }
 
 }
